7e9faec6aba947100b22e943d6128a57a5744bf1
tanti100 <fihartanti94@gmail.com>
2017-03-24 19:14:29 +0700

    tugas
    Information_retrieval

---
+++ upload.php
@@ -0,0 +1,12 @@
+<html>
+<title>Upload PDF</title>
+<body>
+<center>
+<form enctype="multipart/form-data" method="POST" action="hasil_upload.php">
+File  :<input type="file" name="fupload"><br>
+Deskripsi File : <br>
+<textarea name="deskripsi" rows="8" cols="40"></textarea><br>
+<input type=submit value=Upload>
+</form>
+</center>
+</body>
